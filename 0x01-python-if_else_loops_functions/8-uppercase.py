de9d02fb82fb451baee1be1d04ba205a323b91e8
Anthony Baah <abaah17@alustudent.com>
2021-01-11 17:39:14 +0200

    Conditional Operations

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for car in str:
         if 97 <= ord(car) <= 122:
             car = chr(ord(car) - 32)
-            print("{}".format(car), end="")
+        print("{}".format(car), end="")
     print()
