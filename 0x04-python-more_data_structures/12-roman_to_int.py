384a300e42c0d905c6a98572a0ccf0b520ee5393
Anthony Baah <abaah17@alustudent.com>
2021-01-28 16:05:06 +0200

    Data-Structures: Sets and Dictionaries concepts

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
@@ -7,7 +9,6 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
         roman_string is None):
         return (0)
-
 
     for i in range(len(roman_string)):
         if roman_dict.get(roman_string[i], 0) == 0:
