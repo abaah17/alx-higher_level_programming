dccaec87f222c5f783c5432588d73f1d7510fb7d
Anthony Baah <abaah17@alustudent.com>
2021-02-01 18:10:41 +0200

    Error handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     real_n = 0
-    for i in range(0,x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             real_n += 1
