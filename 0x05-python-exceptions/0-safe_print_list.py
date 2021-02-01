ee0378417b4f76d7e39a4502f8eb395d9bec75a7
Anthony Baah <abaah17@alustudent.com>
2021-02-01 18:03:57 +0200

    Error handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
             real_n += 1
         except IndexError:
             break
-        print("")
-        return real_n
+    print("")
+    return real_n
