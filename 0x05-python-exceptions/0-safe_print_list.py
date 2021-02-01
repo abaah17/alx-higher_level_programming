7aad8b6e1ed672d3159a7e5a5c52405b896228ce
Anthony Baah <abaah17@alustudent.com>
2021-02-01 18:02:20 +0200

    Error handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     real_n = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             real_n += 1
         except IndexError:
             break
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     real_n = 0
     for i in range(0,x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             real_n += 1
         except (ValueError, TypeError):
             continue
